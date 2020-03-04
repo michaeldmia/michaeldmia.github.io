007ea5005914d0932990776c31baf0d843a90a9b
Michael Miano <m_miano@massbay.edu>
2020-03-04 17:26:34 -0500

    New Site

--- projects/texas-star/README.txt
+++
@@ -1,4 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/michaeldm/pen/eamWYa.
-
- jquery to add/remove class to change styles on element when scrolled past defined height. 
-taken from http://jsfiddle.net/ult_combo/ayGwn/4/
---
+++ script/script.js
@@ -0,0 +1,29 @@
+﻿
+$(function(){
+    createSticky($(".header"));
+});
+function createSticky(sticky) {
+    
+    if (typeof sticky !== "undefined") {
+
+        var pos = sticky.offset().top + 105,
+                win = $(window);
+            
+        win.on("scroll", function() {
+            win.scrollTop() >= pos ? sticky.addClass("header-active") : sticky.removeClass("header-active");      
+        });         
+    }
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
