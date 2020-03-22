ab9047b15dfddc20fc070d1ee70daeecfca2018d
Michael Miano <m_miano@massbay.edu>
2020-03-22 14:59:34 -0400

    delete beta

--- kinochronika/Script/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("responsive-nav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
