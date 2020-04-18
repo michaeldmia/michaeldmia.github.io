fdfbe64d449ef37922c005580c65ba586c4054d5
Michael Miano <m_miano@massbay.edu>
2020-04-18 11:07:04 -0400

    Update

---
+++ beta/case/Script/index.js
@@ -0,0 +1,43 @@
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "main-site-nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "main-site-nav";
+  }
+}
+function expandMenu1() {
+  document.getElementById("dropdown-1").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsById("dropdown-1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function expandMenu2() {
+  document.getElementById("dropdown-2").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsById("dropdown-2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
